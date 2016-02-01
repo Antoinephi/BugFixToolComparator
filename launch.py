8f29e601f0869dadf01d6dc0bf1bf4d625d3f20e
Mandrivia <antoine.philippe62@gmail.com>
2016-02-01 15:24:34 +0100

    fix bug script + timeout 5min + process bloquants

--- launch.py
+++ launch.py
@@ -1,6 +1,10 @@
 # !/usr/bin/python3.4
 import sys
 import os
+import subprocess
+import time
+
+start_time = time.time()
 
 #argv[1] = IntroClassJava dir's path
 for dirname, dirnames, filenames in os.walk(sys.argv[1]):
@@ -8,4 +12,13 @@ for dirname, dirnames, filenames in os.walk(sys.argv[1]):
   for subdirname in dirnames:
     if subdirname == 'src' :
       print(os.path.join(dirname, subdirname))
-      os.popen('java -jar fixit-1-jar-with-dependencies.jar ' + os.path.join(dirname, subdirname))
+      subprocess.call('java -jar fixit-1-jar-with-dependencies.jar ' + os.path.join(dirname, subdirname), shell=True, timeout=300)
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+###Fixit
+
+###Nopol
+
+###Astor
