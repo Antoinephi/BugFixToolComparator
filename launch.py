76638d19e4856afd98d11280c85a24c168809139
Mandrivia <antoine.philippe62@gmail.com>
2016-02-05 18:00:25 +0100

    fixit function

--- launch.py
+++ launch.py
@@ -11,13 +11,17 @@ for dirname, dirnames, filenames in os.walk(sys.argv[1]):
   # print path to all subdirectories first.
   for subdirname in dirnames:
     if subdirname == 'src' :
-      print(os.path.join(dirname, subdirname))
-      subprocess.call('java -jar fixit-1-jar-with-dependencies.jar ' + os.path.join(dirname, subdirname), shell=True, timeout=300)
+      # print(os.path.join(dirname, subdirname))
+      launch_fixit(os.path.join(dirname, subdirname))
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 ###Fixit
+
+def launch_fixit(path):
+  print(path)
+  subprocess.call('java -jar fixit-1-jar-with-dependencies.jar ' + path, shell=True, timeout=300)
 
 ###Nopol
 
