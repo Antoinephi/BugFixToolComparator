6db3667e01ed381c59aad016bf7a7e6a4c002935
Mandrivia <antoine.philippe62@gmail.com>
2016-02-05 12:46:17 +0100

    refactor script + add mvn install for every introClassProject with Astor

--- launch.py
+++ launch.py
@@ -4,21 +4,34 @@ import os
 import subprocess
 import time
 
-start_time = time.time()
-
-#argv[1] = IntroClassJava dir's path
-for dirname, dirnames, filenames in os.walk(sys.argv[1]):
-  # print path to all subdirectories first.
-  for subdirname in dirnames:
-    if subdirname == 'src' :
-      print(os.path.join(dirname, subdirname))
-      subprocess.call('java -jar fixit-1-jar-with-dependencies.jar ' + os.path.join(dirname, subdirname), shell=True, timeout=300)
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
-
 ###Fixit
+
+def fixit(path):
+	print(path)
+	subprocess.call('java -jar fixit-1-jar-with-dependencies.jar ' + path, shell=True, timeout=300)
+
 
 ###Nopol
 
 ###Astor
+
+def astor(dirname):
+	subprocess.call('mvn install -f ' + dirname + '/pom.xml', shell=True, timeout=300)
+
+
+start_time = time.time()
+
+# argv[1] = IntroClassJava dir's path
+for dirname, dirnames, filenames in os.walk(sys.argv[1]):
+  # print path to all subdirectories first.
+  for subdirname in dirnames:
+    if subdirname == 'src' :
+    	if sys.argv[2] == '-fixit' or sys.argv[2] == '-all':
+    		fixit(os.path.join(dirname, subdirname))
+    	if sys.argv[2] == '-astor' or sys.argv[2] == '-all':
+    		astor(dirname) 
+      
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
