7b83877de3b5d503544789948d092d12e77d821f
Mandrivia <antoine.philippe62@gmail.com>
2016-02-05 18:25:55 +0100

    fonction lancement nopol, ok, reste à gérer les wb/bb

--- launch.py
+++ launch.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import time
 
+##TODO : mvn install nopol pour générer le nouveau jar nopol
+
 ###Fixit
 
 def fixit(path):
@@ -12,6 +14,12 @@ def fixit(path):
 
 
 ###Nopol
+
+def nopol(dirname):
+	subprocess.call('mvn clean test -f ' + dirname + '/pom.xml', shell=True, timeout=300)
+	subprocess.call('mvn package -f ' + dirname + '/pom.xml', shell=True, timeout=300)
+	subprocess.call('java -jar  ../nopol/nopol/target/nopol-0.0.3-SNAPSHOT-jar-with-dependencies.jar -p ../nopol/nopol/lib/z3/z3_for_linux -s ' + dirname + ' -c ' + dirname + '/target/classes:' + dirname +'/target/test-classes:junit-4.11.jar', shell=True, timeout=300)	
+
 
 ###Astor
 
@@ -25,10 +33,14 @@ start_time = time.time()
 
 # argv[1] = IntroClassJava dir's path
 for dirname, dirnames, filenames in os.walk(sys.argv[1]):
-  # print path to all subdirectories first.
-  for subdirname in dirnames:
-    if subdirname == 'src' :
-    	if sys.argv[2] == '-fixit' or sys.argv[2] == '-all':
-    		fixit(os.path.join(dirname, subdirname))
-    	if sys.argv[2] == '-astor' or sys.argv[2] == '-all':
-    		astor(dirname) 
+	# print path to all subdirectories first.
+	for subdirname in dirnames:
+		if subdirname == 'src' :
+			if sys.argv[2] == '-fixit' or sys.argv[2] == '-all':
+				fixit(os.path.join(dirname, subdirname))
+			# if sys.argv[2] == '-astor' or sys.argv[2] == '-all':
+			# 	astor(dirname)
+			if sys.argv[2] == '-nopol' or sys.argv[2] == '-all':
+				nopol(dirname)
+
+print("--- %s seconds ---" % (time.time() - start_time))
